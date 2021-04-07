afe384f1e4a10897c98a66c288f2c02c003197e8
oruc <yusa.oruc@stu.fsm.edu.tr>
2021-04-07 17:23:21 +0300

    first commit

---
+++ Assets/AnaCember.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnaCember : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject kucukCember;
+    GameObject OyunYoneticisi;
+    void Start()
+    {
+        OyunYoneticisi = GameObject.FindGameObjectWithTag("oyunyoneticisiTag");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            kucukCemberOlustur();
+        }
+    }
+    void kucukCemberOlustur(){
+        Instantiate(kucukCember, transform.position, transform.rotation);
+        OyunYoneticisi.GetComponent<OyunYoneticisiKod>().KucukCemberlerdeTextGosterme();
+    }
+}
